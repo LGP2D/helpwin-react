b5d2fa76c59bd45b55095599134d0b075811fee8
Pedro <pedroob221@gmail.com>
2017-04-05 10:52:53 +0100

    Demo updated

---
+++ src/routes/Test/components/TestView.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import DuckImage from '../assets/Duck.jpg'
+import './HomeView.scss'
+
+export const HomeView = () => (
+  <div>
+    <h4>Welcome to the test!</h4>
+  </div>
+);
+
+export default HomeView

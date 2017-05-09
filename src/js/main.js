dcb5c9ce2b17553616acbb6898b4e28983cbbdb1
Pedro <pedroob221@gmail.com>
2017-05-09 20:35:31 +0100

    Hooking up dashboard

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,11 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import Layout from 'app/components/frontpage/layout/layout-onepage';
 import LayoutBlank from 'app/components/frontpage/layout/layout-blank';
 import FrontHome from 'app/pages/frontpage/home';
-import DashHome from 'app/pages/dashboard/home';
 import Register from 'app/pages/frontpage/register';
 import VolunteeringList from 'app/pages/frontpage/volunteering-list';
+
+import DashLayout from 'app/components/dashboard/layout/layout'
+import DashHome from 'app/pages/dashboard/home';
 
 const app = document.getElementById('app');
 
@@ -16,7 +18,7 @@ ReactDOM.render(
         <Route path='/' component={ Layout }>
             <IndexRoute component={ FrontHome }/>
         </Route>
-        <Route path='/dashboard' component={ Layout }>
+        <Route path='/dashboard' component={ DashLayout }>
             <IndexRoute component={ DashHome }/>
         </Route>
         <Route path='/register' component={ LayoutBlank }>

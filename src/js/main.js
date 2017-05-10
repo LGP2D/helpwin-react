c35b5fe73e2f417c05cf5157640485feb55a23cb
Nuno Matos <nunobarros_@hotmail.com>
2017-05-10 11:01:57 +0100

    auth guards

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, hashHistory, Redirect } from 'react-router';
 
 import Layout from 'app/components/frontpage/layout/layout-onepage';
 import LayoutBlank from 'app/components/frontpage/layout/layout-blank';
@@ -10,15 +10,29 @@ import Register from 'app/pages/frontpage/register';
 import VolunteeringList from 'app/pages/frontpage/volunteering-list';
 import Login from 'app/pages/frontpage/login';
 
+import UserStore from 'app/stores/userStore';
+
 const app = document.getElementById('app');
 
+export default function requireAuth (nextState, replace) {
+    let loggedIn = UserStore.isLoggedIn();
+    if (!loggedIn) {
+        replace({
+            pathname: '/login',
+            state: { nextPathname: nextState.location.pathname }
+        });
+    }
+};
+/**
+ * Auth guard is not on any of the pages as of right now.
+ * If you wish to use it, just type onEnter on the route and direct it to { requireAuth }
+ * E.g <IndexRoute component={ VolunteeringList } onEnter={ requireAuth }
+ */
 ReactDOM.render(
     <Router history={ hashHistory }>
         <Route path='/' component={ Layout }>
             <IndexRoute component={ FrontHome }/>
-        </Route>
-        <Route path='/dashboard' component={ Layout }>
-            <IndexRoute component={ DashHome }/>
+            <Route path='/dashboard' component={ DashHome }/>
         </Route>
         <Route path='/register' component={ LayoutBlank }>
             <IndexRoute component={ Register }/>
--- src/js/pages/frontpage/volunteering-list.js
+++ src/js/pages/frontpage/volunteering-list.js
@@ -99,4 +99,4 @@ export default class VolunteeringList extends React.Component {
             />
         );
     }
-}
+};

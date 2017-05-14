51c16f3ede3b9293c72f02f00e7ad407a04c1903
Pedro <pedroob221@gmail.com>
2017-05-14 21:47:53 +0100

    Dashboard Volunteering Proposals

--- src/js/pages/dashboard/volunteerProposals.js
+++ src/js/pages/dashboard/volunteerProposals.js
@@ -1,28 +1,99 @@
 import React from 'react';
-import UserStore from  'app/stores/userStore';
-import LabelForm from 'app/components/frontpage/forms/FormLabel';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+
+import * as VolunteeringActions from 'app/actions/volunteeringListActions';
+import VolunteeringStore from 'app/stores/volunteeringListStore';
+
+import 'react-bootstrap-table/dist/react-bootstrap-table.min.css';
 
 export default class VolunteerProposals extends React.Component {
-    constructor (){
+    constructor () {
         super();
+
+        this.state = {
+            data: []
+        };
+    }
+
+    componentWillMount () {
+        VolunteeringStore.on('update', this.updateTable);
+        VolunteeringActions.fetchData();
+    }
+
+    componentWillUnmount () {
+        VolunteeringStore.removeListener('update', this.updateTable);
     }
 
     render () {
         const { location } = this.props;
 
+        console.log(this.state.data);
+
         return (
 
-            <div className='panel profile-panel-border'>
+            <div class='panel panel-headline'>
                 <div class='panel-heading'>
-                    <h3 class='panel-title'>
-                        Proposals
-                    </h3>
+                    <h3 class='panel-title'>Volunteering Proposals</h3>
+                    <p class='panel-subtitle'>A list with all registered institutions</p>
+                    <div class='right'>
+                        <button type='button' class='btn-toggle-collapse'>
+                            <i class='ti ti-angle-up' />
+                        </button>
+                        <button type='button' class='btn-remove'>
+                            <i class='ti ti-close' />
+                        </button>
+                    </div>
                 </div>
-                <div className='panel-body'>
-                    Hey
+                <div class='panel-body'>
+                    <BootstrapTable data={ this.state.data } striped={ true } hover={ true }>
+                        <TableHeaderColumn dataField='institution' dataFormat={ this.imageFormatter }
+                                           isKey={ true }>
+                            Logo
+                        </TableHeaderColumn>
+                        <TableHeaderColumn dataField='institution' dataFormat={ this.nameFormatter }>
+                            Name
+                        </TableHeaderColumn>
+                        <TableHeaderColumn dataFormat={ this.locationDateFormatter }>
+                            Location & Date
+                        </TableHeaderColumn>
+                        <TableHeaderColumn dataField='description'>
+                            Description
+                        </TableHeaderColumn>
+                    </BootstrapTable>
                 </div>
             </div>
 
+        );
+    }
+
+    updateTable = () => {
+        this.setState({
+            data: VolunteeringStore.getAll()
+        });
+    };
+
+    imageFormatter (cell, row) {
+        return (
+            <img height='50' src={ cell.imageUrl } />
+        );
+    }
+
+    nameFormatter (cell, row) {
+        return (
+            cell.name
+        );
+    }
+
+    locationDateFormatter (cell, row) {
+        return (
+            <div className='text-center'>
+                <p> { row.location } </p>
+                <i className='fa fa-calendar'/><span
+                className='volunteering-table-text-margin'>Starting: { row.dateStart }</span>
+                <br />
+                <i className='fa fa-calendar'/><span
+                className='volunteering-table-text-margin'>Ending: { row.dateEnd }</span>
+            </div>
         );
     }
 }
